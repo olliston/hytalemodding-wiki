9eeb738088dc92ac3bea4cc462f9590451de8a3c
ItsNeil17 <neil@hytalemodding.dev>
2026-03-02 00:41:19 +0530

    feat: add api middleware to app
    Signed-off-by: ItsNeil17 <neil@hytalemodding.dev>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AuthenticateAPIKey;
+use App\Http\Middleware\EnsureAPIScope;
 use App\Http\Middleware\EnsureEmailIsVerified;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
@@ -11,6 +13,7 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -19,12 +22,17 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified.email' => EnsureEmailIsVerified::class,
+            'api.scope' => EnsureAPIScope::class,
         ]);
 
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        $middleware->api(append: [
+            AuthenticateAPIKey::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
